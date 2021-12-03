81bbab9cf76c8b1f20a94f082d186cad4f340098
Sean Horner <sean.w.horner@gmail.com>
2021-12-03 01:36:08 -0600

    Updated to version 0.2.6

--- build_and_push_tool.sh
+++ build_and_push_tool.sh
@@ -1,142 +1,76 @@
 #!/bin/bash
 
+
+# Version Markers
 VER_MAJ=0
-VER_MIN=1
-VER_PATCH=0
+VER_MIN=2
+VER_PATCH=6
 
-##### ECR login
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/b9s2q8s8
+########################################################################################################################
+#######################################                                         ########################################
+#######################################               FUNCTIONS                 ########################################
+#######################################                                         ########################################
+########################################################################################################################
 
-##### Functions
+
+# ECR login
+# Requires AWS-CLI to be installed and configured with a profile with appropriate permissions.
+login()
+{
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/b9s2q8s8
+}
+
 
 # Usage utility
 usage()
 {
+  echo
   echo "WARNING: This utility was written for my particular environment, but it should be "
   echo "         straightforward enough to easily bend to your use case."
   echo
-  echo
-  #  Compact style of
-  #  echo "usage:  build_and_push_tool ["
-  #  echo "                              [-a | --admin ver_num]"
-  #  echo "                              [-b | --bookings ver_num]"
-  #  echo "                              [-d | --data_prod ver_num]"
-  #  echo "                              [-f | --flights ver_num]"
-  #  echo "                              [-u | --users ver_num]"
-  #  echo "                            ]"
   echo "NAME:"
   echo "  Sean's Utopia Project Build and Push Tool "
+  echo
   echo "USAGE:"
-  echo "  build_and_push_tool [image_flags] image_version_number"
+  echo "  build_and_push_tool [aux_flags] [image_flags image_version_number]"
+  echo
   echo "VERSION:"
   echo "  $VER_MAJ.$VER_MIN.$VER_PATCH "
+  echo
   echo "IMAGE FLAGS:"
+  echo " *Note*: Calling a flag requires a version number to tag the image with."
   echo "  -a | --admin            Frontend Admin Microservice"
   echo "  -b | --bookings         Backend Bookings Microservice"
   echo "  -d | --data_prod        Backend Data Producers Microservice"
   echo "  -f | --flights          Backend Flights Microservice"
   echo "  -u | --users            Backend Users Microservice"
-  echo "NOTE: calling a flag requires a version number to tag the image with."
+  echo
+  echo "AUXILIARY FLAGS:"
+  echo "  -h | --help             Displays this usage message for this tool."
+  echo "  -v | --version          Displays the tool's version in MAJ.MIN.PATCH format."
+  echo "  -l | --login            Gets ECR credentials for pushing to ECR (req. AWS-CLI)."
+  echo
 }
+
 
 # Function to test that the appropriate type of argument is passed to each builder
 param_tester()
 {
-  if [ $1 == "" || ${$1:0:1} == "-" ]; then
+  param="$1"
+  first_letter="${param:0:1}"
+  if [ "$param" == "" ] || [ "$first_letter" == "-" ]; then
     echo "Every image builder requires a version number, please provide one for $2."
-    usage
+    echo "Please use the -h or --help flag to see appropriate usage."
     exit 1
   fi
 }
 
-# Bookings microservice build and push utility
-booking_build_and_push()
-{
-  param_tester $1 -b
-
-  docker build \
-    -t utopia_backend_bookings_microservice:v$1 \
-    -t seanhorner/utopia_backend_bookings_microservice:v$1 \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:v$1 \
-    -t utopia_backend_bookings_microservice:latest \
-    -t seanhorner/utopia_backend_bookings_microservice:latest \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:latest \
-    -f backend/bookings_dockerfile \
-    backend/microservice_bookings
-
-  docker push seanhorner/utopia_backend_bookings_microservice:v$1
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:v$1
-  docker push seanhorner/utopia_backend_bookings_microservice:latest
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:latest
-}
-
-# Flights microservice build and push utility
-flights_build_and_push()
-{
-  param_tester $1 -f
-
-  docker build \
-    -t utopia_backend_flights_microservice:v$1 \
-    -t seanhorner/utopia_backend_flights_microservice:v$1 \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:v$1 \
-    -t utopia_backend_flights_microservice:latest \
-    -t seanhorner/utopia_backend_flights_microservice:latest \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:latest \
-    -f backend/flights_dockerfile \
-    backend/microservice_flights
-
-  docker push seanhorner/utopia_backend_flights_microservice:v$1
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:v$1
-  docker push seanhorner/utopia_backend_flights_microservice:latest
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:latest
-}
-# Users microservice build and push utility
-users_build_and_push()
-{
-  param_tester $1 -u
-
-  docker build \
-    -t utopia_backend_users_microservice:v$1 \
-    -t seanhorner/utopia_backend_users_microservice:v$1 \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:v$1 \
-    -t utopia_backend_users_microservice:latest \
-    -t seanhorner/utopia_backend_users_microservice:latest \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:latest \
-    -f backend/users_dockerfile \
-    backend/microservice_users
-
-  docker push seanhorner/utopia_backend_users_microservice:v$1
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:v$1
-  docker push seanhorner/utopia_backend_users_microservice:latest
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:latest
-}
-
-# Data producers microservice build and push utility
-data_producers_build_and_push()
-{
-  param_tester $1 -d
-
-  docker build \
-    -t utopia_backend_data_producers_microservice:v$1 \
-    -t seanhorner/utopia_backend_data_producers_microservice:v$1 \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:v$1 \
-    -t utopia_backend_data_producers_microservice:latest \
-    -t seanhorner/utopia_backend_data_producers_microservice:latest \
-    -t public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:latest \
-    -f backend/data_producers_dockerfile \
-    backend/microservice_data_producers
-
-  docker push seanhorner/utopia_backend_data_producers_microservice:v$1
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:v$1
-  docker push seanhorner/utopia_backend_data_producers_microservice:latest
-  docker push public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:latest
-}
 
 # Admin microservice build and push utility
 admin_build_and_push()
 {
-  param_tester $1 -a
+  param_tester "$1" "-a"
 
   docker build \
     -t utopia_frontend_admin_microservice:v$1 \
@@ -155,7 +89,106 @@ admin_build_and_push()
 }
 
 
-##### Main Parser
+# Bookings microservice build and push utility
+booking_build_and_push()
+{
+  param_tester "$1" "-b"
+
+  docker build \
+    -t utopia_backend_bookings_microservice:v$1 \
+    -t seanhorner/utopia_backend_bookings_microservice:v$1 \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:v$1 \
+    -t utopia_backend_bookings_microservice:latest \
+    -t seanhorner/utopia_backend_bookings_microservice:latest \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:latest \
+    -f backend/bookings_dockerfile \
+    backend/microservice_bookings
+
+  docker push seanhorner/utopia_backend_bookings_microservice:v$1
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:v$1
+  docker push seanhorner/utopia_backend_bookings_microservice:latest
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_bookings_microservice:latest
+}
+
+
+# Data producers microservice build and push utility
+data_producers_build_and_push()
+{
+  param_tester "$1" "-d"
+
+  docker build \
+    -t utopia_backend_data_producers_microservice:v$1 \
+    -t seanhorner/utopia_backend_data_producers_microservice:v$1 \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:v$1 \
+    -t utopia_backend_data_producers_microservice:latest \
+    -t seanhorner/utopia_backend_data_producers_microservice:latest \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:latest \
+    -f backend/data_producers_dockerfile \
+    backend/microservice_data_producers
+
+  docker push seanhorner/utopia_backend_data_producers_microservice:v$1
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:v$1
+  docker push seanhorner/utopia_backend_data_producers_microservice:latest
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_data_producers_microservice:latest
+}
+
+
+# Flights microservice build and push utility
+flights_build_and_push()
+{
+  param_tester "$1" "-f"
+
+  docker build \
+    -t utopia_backend_flights_microservice:v$1 \
+    -t seanhorner/utopia_backend_flights_microservice:v$1 \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:v$1 \
+    -t utopia_backend_flights_microservice:latest \
+    -t seanhorner/utopia_backend_flights_microservice:latest \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:latest \
+    -f backend/flights_dockerfile \
+    backend/microservice_flights
+
+  docker push seanhorner/utopia_backend_flights_microservice:v$1
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:v$1
+  docker push seanhorner/utopia_backend_flights_microservice:latest
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_flights_microservice:latest
+}
+
+
+# Users microservice build and push utility
+users_build_and_push()
+{
+  param_tester "$1" "-u"
+
+  docker build \
+    -t utopia_backend_users_microservice:v$1 \
+    -t seanhorner/utopia_backend_users_microservice:v$1 \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:v$1 \
+    -t utopia_backend_users_microservice:latest \
+    -t seanhorner/utopia_backend_users_microservice:latest \
+    -t public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:latest \
+    -f backend/users_dockerfile \
+    backend/microservice_users
+
+  docker push seanhorner/utopia_backend_users_microservice:v$1
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:v$1
+  docker push seanhorner/utopia_backend_users_microservice:latest
+  docker push public.ecr.aws/b9s2q8s8/utopia_backend_users_microservice:latest
+}
+
+
+tester_func()
+{
+  echo "Ran the tester with param $1"
+}
+
+
+########################################################################################################################
+#######################################                                         ########################################
+#######################################           MAIN PARSER LOOP              ########################################
+#######################################                                         ########################################
+########################################################################################################################
+
 while [ "$1" != "" ]; do
   case $1 in
     -a | --admin )        shift
@@ -179,6 +212,15 @@ while [ "$1" != "" ]; do
     -v | --version )      echo "version: $VER_MAJ.$VER_MIN.$VER_PATCH "
                           exit
                           ;;
+    -l | --login )        login
+                          ;;
+    -t )                  if [ "$2" == "" ] || [ ${"$2":0:1} == "-" ]; then
+                            tester_func
+                          else
+                            tester_func "$2"
+                            shift
+                          fi
+                          ;;
     * )                   usage
                           exit 1
                           ;;
@@ -186,4 +228,4 @@ while [ "$1" != "" ]; do
   shift
 done
 
-echo "Completed"
+echo "Successfully completed build and push."
